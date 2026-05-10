a9126ac2fed39f902bde602dbc883492b3936234
ferreirajoao-fatec <joao.ferreira11>
2026-05-09 21:20:00 -0300

    7-agendamento-e-conclusão-de-adoção-schedule-e-concludeadoption

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -207,9 +207,71 @@ module.exports = class PetController {
         res.status(200).json({ message: 'Pet atualizado com sucesso!', data: updateData })
     }
     static async schedule(req, res) {
-        res.status(200).json({ message: 'em breve....' })
+        const id = req.params.id
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'O id do pet é inválido' })
+            return
+        }
+
+        const pet = await Pet.findById(id)
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' })
+            return
+        }
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if (pet.user._id.toString() === user._id.toString()) {
+            res.status(403).json({ message: 'Você não pode agendar uma visita com seu próprio pet!' })
+            return
+        }
+
+        pet.adopter = {
+            _id: user._id,
+            name: user.name,
+            image: user.image
+        }
+
+        try {
+            await Pet.findByIdAndUpdate(id, pet)
+            return res.status(200).json({ message: 'Visita agendada com sucesso!' })
+        } catch (error) {
+            return res.status(503).json({ message: error })
+        }
     }
     static async concludeAdoption(req, res) {
-       res.status(200).json({ message: 'em breve....' })
+       const id = req.params.id
+
+       if (!mongoose.Types.ObjectId.isValid(id)) {
+           res.status(422).json({ message: 'O id do pet é inválido' })
+           return
+       }
+
+         const pet = await Pet.findById(id)
+
+         if (!pet) {
+             res.status(404).json({ message: 'Pet não encontrado!' })
+             return
+         }
+
+            const token = getToken(req)
+            const user = await getUserByToken(token)
+
+            if (pet.user._id.toString() !== user._id.toString()) {
+                res.status(403).json({ message: 'Acesso Negado' })
+                return
+            }
+
+            pet.available = false
+
+            try {
+                await Pet.findByIdAndUpdate(id, pet)
+                return res.status(200).json({ message: 'Adoção concluída com sucesso!' })
+            } catch (error) {
+                return res.status(503).json({ message: error })
+            }    
     }
 }
